4ee6e39f2af8d25aa0d6d3a03378a31cdd857724
HOME-CARLES\Carles <developercarles@gmail.com>
2018-02-08 17:32:20 +0100

    modificacion de la pagina root y navegar entre paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,27 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { 
+  Pagina2Page, 
+  Pagina3Page, 
+  AjustesPage, 
+  Ajustes2Page, 
+  TabsPage, 
+  ModalPage, 
+  PrincipalPage 
+} from "../pages/index.paginas";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    Pagina2Page, 
+    Pagina3Page, 
+    AjustesPage, 
+    Ajustes2Page, 
+    TabsPage, 
+    ModalPage, 
+    PrincipalPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +34,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    Pagina2Page, 
+    Pagina3Page, 
+    AjustesPage, 
+    Ajustes2Page, 
+    TabsPage, 
+    ModalPage, 
+    PrincipalPage
   ],
   providers: [
     StatusBar,
--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+
+import { Pagina2Page } from "../index.paginas";
 
 @Component({
   selector: 'page-principal',
   templateUrl: 'principal.html',
 })
 export class PrincipalPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  pagina2: any = Pagina2Page;
+  constructor(public navCtrl: NavController) {
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PrincipalPage');
+  navegarPagina(){
+    this.navCtrl.push(Pagina2Page);
   }
 
 }
