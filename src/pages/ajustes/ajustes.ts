9b5c15ea93034d1f77c081e149c0915f3c59dc65
HOME-CARLES\Carles <developercarles@gmail.com>
2018-02-10 10:54:55 +0100

    crear modal y cerrar modal con params, cerrar modal sin params

--- src/pages/ajustes/ajustes.ts
+++ src/pages/ajustes/ajustes.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController } from 'ionic-angular';
+import { ModalPage } from "../index.paginas";
 
-/**
- * Generated class for the AjustesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-ajustes',
   templateUrl: 'ajustes.html',
 })
 export class AjustesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController ) {
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AjustesPage');
+  /* 
+  @param 1 página a navegar
+  */
+  activarPrincipal(){
+    this.navCtrl.parent.select(1); //Retorno a la pagina deseada 
   }
-
+  mostrar_modal(){
+    let modal = this.modalCtrl.create(ModalPage, {nombre: 'carles', edad: 40});
+    modal.present();
+    modal.onDidDismiss(paramertros =>{
+      if (paramertros) {
+        console.log('Params del modal: ', paramertros);
+      }else{
+        console.log(('cerrar sin parametros'));
+      }
+    });
+  }
 }
